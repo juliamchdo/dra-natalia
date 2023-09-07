ce4cb5c0a3e33360197affda27bca68ee54b1912
julia <juliamachado226@gmail.com>
2023-09-07 19:17:23 -0300

    Add text to the cards

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,29 +1,62 @@
 import "@/app/style/card.scss";
-export function Card(){
+
+export function Card() {
 
     const images = [
-        {id: 'photo-1', frontText: 'Diabetes', backText: 'teste'},
-        {id: 'photo-2', frontText: 'Obesidade', backText: 'teste'},
-        {id: 'photo-3', frontText: 'Osteoporose e Distúrbios Ósseos', backText: 'teste'},
-        {id: 'photo-4', frontText: 'Distúrbios da Tireóide', backText: 'teste'},
-        {id: 'photo-5', frontText: 'Menopausa e Climatério', backText: 'teste'},
-        {id: 'photo-6', frontText: 'Andropausa', backText: 'teste'},
-        {id: 'photo-7', frontText: 'Doenças da Hipófise e Adrenais', backText: 'teste'},
-        {id: 'photo-8', frontText: 'Reposição Hormonal em Transgêneros', backText: 'teste'},
+        {
+            id: 'photo-1',
+            title: 'Diabetes',
+            text: 'Atendimento de pessoas com diabetes, ajuste de doses de insulinas e rastreio de complicações micro e macrovasculares'
+        },
+        {
+            id: 'photo-2',
+            title: 'Obesidade',
+            text: 'Emagrecimento com saúde, visando mudanças de hábitos e planos de tratamentos inovadores'
+        },
+        {
+            id: 'photo-3',
+            title: 'Osteoporose e Distúrbios Ósseos',
+            text: 'Doenças em que ocorrem perda de massa óssea e risco de fraturas, e necessitam de tratamento e dieta adequados'
+        },
+        {
+            id: 'photo-4',
+            title: 'Distúrbios da Tireóide',
+            text: 'Tratamento de nódulos tireoidianos, hipertireoidismo, hipotireoidismo, Tireoidite de Hashimoto e Doença de Graves'
+        },
+        {
+            id: 'photo-5',
+            title: 'Menopausa e Climatério',
+            text: 'Reposição hormonal para mulheres que sentem os sinais e sintomas da menopausa: ondas de calor, secura vaginal, baixa libido, transpiração, tonturas, palpitações, suores noturnos prejudicando o sono; depressão ou irritabilidade'
+        },
+        {
+            id: 'photo-6',
+            title: 'Andropausa',
+            text: 'Reposição hormonal masculina quando ocorre queda na produção dos hormônios androgênicos'
+        },
+        {
+            id: 'photo-7',
+            title: 'Doenças da Hipófise e Adrenais',
+            text: 'Tumores hipofisários que podem produzir hormônios ou não. Tratamento da acromegalia, doença de cushing e prolactinomas.'
+        },
+        {
+            id: 'photo-8',
+            title: 'Reposição Hormonal em Transgêneros',
+            text: 'Terapia de reposição hormonal para modificações corpóreas'
+        },
     ]
 
-    return(
+    return (
         images.map(image => {
-            return(
+            return (
                 <div className="card">
                     <div className="card-inner">
                         <div className="card-front flex justify-center items-center">
-                            <p className="card-text z-50">{image.frontText}</p>
+                            <h2 className="card-title z-50">{image.title}</h2>
                             <div className={"card-photo " + image.id}></div>
                         </div>
 
-                        <div className="card-back flex justify-center items-center">
-                            <p className="card-text z-50">{image.backText}</p>
+                        <div className="card-back px-3 flex justify-center items-center">
+                            <h3 className="card-text">{image.text}</h3>
                         </div>
                     </div>
                 </div>
